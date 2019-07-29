4b4a817e0da7ab5bff88a470186aaee7386fb00b
Mo <Gabalyma@gmail.com>
2019-07-29 01:37:48 -0700

    scraping

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -13,38 +13,41 @@ exports.index = function(req, res) {
 };
 
 exports.scrape = function(req, res) {
-  axios.get("http://www.echojs.com/").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
+  axios
+    .get("https://business.financialpost.com/author/bloombergnp")
+    .then(function(response) {
+      // Then, we load that into cheerio and save it to $ for a shorthand selector
+      var $ = cheerio.load(response.data);
 
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
-      // Save an empty result object
-      var result = {};
+      // Now, we grab every h2 within an article tag, and do the following:
+      $("h4.gallery-title").each(function(i, element) {
+        // Save an empty result object
+        var result = {};
+        // Add the text and href of every link, and save them as properties of the result object
+        result.title = $(this)
+          .children("a")
+          .text();
+        result.link = $(this)
+          .children("a")
+          .attr("href");
+        result.summary = $(this)
+          .children("a")
+          .text();
 
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
-
-      // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-        .then(function(dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-          // If an error occurred, log it
-          console.log(err);
-        });
+        // Create a new Article using the `result` object built from scraping
+        db.Article.create(result)
+          .then(function(dbArticle) {
+            // View the added result in the console
+            console.log(dbArticle);
+          })
+          .catch(function(err) {
+            // If an error occurred, log it
+            console.log(err);
+          });
+      });
+      // Send a message to the client
+      res.send("Scrape Complete");
     });
-
-    // Send a message to the client
-    res.send("Scrape Complete");
-  });
 };
 
 exports.articles = function(req, res) {
--- routes/index.js
+++ routes/index.js
@@ -10,14 +10,4 @@ router.get("/scrape", index_controller.scrape);
 
 router.get("/articles", index_controller.articles);
 
-/* GET home page. */
-// router.get("/", function(req, res, next) {
-//     const hbsObject = {
-//       title: "News Scraper",
-//       mainpage: true,
-//     };
-//     res.render("index", hbsObject);
-
-// });
-
 module.exports = router;
